e3bbb9d6bd9af24b000c2344a506703a4690dfcf
ResadCoder <resadsadiqov62@gamil.com>
2025-09-13 05:04:33 +0400

    b njkn

--- WebApplication6/Program.cs
+++ WebApplication6/Program.cs
@@ -3,22 +3,23 @@ using WebApplication6.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Read MySQL URL from environment variable
+// 1. Read the public MySQL URL from environment variable
 var mysqlUrl = Environment.GetEnvironmentVariable("MYSQL_URL1");
 
 if (string.IsNullOrEmpty(mysqlUrl))
 {
-    throw new InvalidOperationException("Database connection string not found in environment variable 'MYSQL_URL1'.");
+    throw new InvalidOperationException(
+        "Database connection string not found in environment variable 'MYSQL_URL1'. " +
+        "Make sure you set it to your Railway public MySQL URL."
+    );
 }
 
 // 2. Convert mysql:// URI to EF Core connection string
-// mysql://user:password@host:port/database
 var uri = new Uri(mysqlUrl);
-var userInfo = uri.UserInfo.Split(':');
-
+var userInfo = uri.UserInfo.Split(':'); // ["root", "password"]
 var efConnectionString = $"Server={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};User={userInfo[0]};Password={userInfo[1]}";
 
-// 3. Add services to the container
+// 3. Add services
 builder.Services.AddControllersWithViews();
 
 // 4. Configure DbContext with MySQL
